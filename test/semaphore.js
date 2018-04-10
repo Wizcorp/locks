708f6b3c7b8b6e9844d114683543054e268dc53f
JK Kim <jung.kim.3627@gmail.com>
2018-04-09 19:22:50 -0700

    Adding initial promise end point

--- test/semaphore.js
+++ test/semaphore.js
@@ -28,9 +28,39 @@ function testMany(t, cb) {
 	}, 0);
 }
 
+function testPromise(t, cb) {
+	var sem = locks.createSemaphore(2);  // 2 available resources
+
+	function fn() {
+		if (Math.random() < 0.5) {
+			process.nextTick(function () {
+				sem.signal();
+			});
+		} else {
+			sem.signal();
+		}
+	}
+
+	var prom = Promise.resolve();
+	for (var i = 0; i < 100; i++) {
+		prom = prom.then(function () {
+			return sem.waitAsync().then(fn);
+		});
+	}
+
+	prom.then(function() {
+		t.equal(sem._count, 2, 'Still 2 available resources');
+		t.equal(sem._waiting.length, 0, 'Nobody waiting');
+		cb();
+	}).catch(function(e) {
+		cb(e);
+	});
+}
 
 test('Semaphore', function (t) {
 	testMany(t, function () {
-		t.end();
+		testPromise(t, function() {
+			t.end();
+		});
 	});
 });
