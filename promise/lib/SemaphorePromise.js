a76ee6ed0128a17e8ee57f43acf5cefb010e7d36
JK Kim <jung.kim.3627@gmail.com>
2018-05-13 17:21:20 -0700

    fix spacing

--- promise/lib/SemaphorePromise.js
+++ promise/lib/SemaphorePromise.js
@@ -1,17 +1,17 @@
 var Semaphore = require('../../lib/Semaphore');
 
 function SemaphorePromise (initialCount) {
-  this._count = initialCount || 1;
+	this._count = initialCount || 1;
 	this._waiting = [];
 }
 
 module.exports = SemaphorePromise;
 
 SemaphorePromise.prototype.wait = function () {
-  var self = this;
-  return new Promise(function (resolve, reject) {
-    self._wait(resolve);
-  });
+	var self = this;
+	return new Promise(function (resolve, reject) {
+		self._wait(resolve);
+	});
 }
 SemaphorePromise.prototype.signal = Semaphore.prototype.signal
 SemaphorePromise.prototype._wait = Semaphore.prototype.wait
--- promise/test/semaphorePromise.js
+++ promise/test/semaphorePromise.js
@@ -2,42 +2,42 @@ var locks = require('../');
 var test = require('tape');
 
 function testPromise(t, cb) {
-  var availableResources = 2;
-  var parallelRunning = 0;
-  var executionCount = 0;
-  var sem = locks.createSemaphore(availableResources);  // 2 available resources
+	var availableResources = 2;
+	var parallelRunning = 0;
+	var executionCount = 0;
+	var sem = locks.createSemaphore(availableResources);	// 2 available resources
 
 	function fn() {
 		if (Math.random() < 0.5) {
 			process.nextTick(function () {
 				sem.signal();
-        parallelRunning--;
+				parallelRunning--;
 			});
 		} else {
 			sem.signal();
-      parallelRunning--;
+			parallelRunning--;
 		}
 	}
 
 	var prom = Promise.resolve();
-  var maxParallel = 0;
+	var maxParallel = 0;
 	for (var i = 0; i < 100; i++) {
 		prom = prom.then(function () {
-      maxParallel = (parallelRunning > maxParallel ? parallelRunning : maxParallel);
-      parallelRunning++;
-      executionCount++;
+			maxParallel = (parallelRunning > maxParallel ? parallelRunning : maxParallel);
+			parallelRunning++;
+			executionCount++;
 			return sem.wait().then(fn);
 		});
 	}
 
-  prom.then(() => {
-    t.ok(maxParallel > 0 && maxParallel <= availableResources, 'Ensure parallelism level is under the resource limit')
-    t.equal(executionCount, 100, 'ensure execution count')
-    t.equal(sem._waiting.length, 0, 'Nobody waiting');
-    cb();
-  }).catch((e) => {
-    cb(e);
-  })
+	prom.then(() => {
+		t.ok(maxParallel > 0 && maxParallel <= availableResources, 'Ensure parallelism level is under the resource limit')
+		t.equal(executionCount, 100, 'ensure execution count')
+		t.equal(sem._waiting.length, 0, 'Nobody waiting');
+		cb();
+	}).catch((e) => {
+		cb(e);
+	})
 }
 
 
